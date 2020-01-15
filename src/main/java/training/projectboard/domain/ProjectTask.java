d2146cd7a653c7a9128c535af151d981e548e5ca
Mateus <mateus.bs.rangel@gmail.com>
2020-01-14 21:31:27 -0300

    added ProjectTask class

--- src/main/java/training/projectboard/domain/ProjectTask.java
+++ src/main/java/training/projectboard/domain/ProjectTask.java
@@ -1,9 +1,55 @@
 package training.projectboard.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
+@Entity     //create de table for this
 public class ProjectTask {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;    // Long wraps a value of the primitive type long in an object
+
+    private String summary;
+    private String acceptanceCriteria;
+    private String status;
+
+    //refact use project lombok maybe
+
+    public ProjectTask() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
